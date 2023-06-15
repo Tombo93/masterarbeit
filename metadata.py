febf774807d8cc30b0b9613adc64fadaf07126d7
Ließmann <bay1989@mario.zbh.uni-hamburg.de>
2023-06-15 18:57:04 +0200

    downloaded ISIC data for family history on remote

--- metadata.py
+++ metadata.py
@@ -2,11 +2,15 @@ import os
 import pandas as pd
 
 
+download_cmd_fx_true = "isic metadata download --search 'family_hx_mm:true' > metadata_fx_true.csv"
+download_cmd_fx_false = "isic metadata download --search 'family_hx_mm:false' > metadata_fx_false.csv"
+
 df1 = pd.read_csv(os.path.join('data/ISIC', 'metadata_fx_false.csv'))
 df2 = pd.read_csv(os.path.join('data/ISIC', 'metadata_fx_true.csv'))
 
 # print(df['diagnosis'].tolist())
 
+# drop this column as it interferes with merging the two files
 df2.drop(columns=['mel_mitotic_index'])
 out = pd.concat([df1, df2])
 # with open('data/ISIC/metadata_combined.csv', 'w', encoding='utf-8') as f:
