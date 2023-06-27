aa5c5859bf5e1af8095c38d82fa3809a8fccfd2b
Tom Ließmann <56519608+Tombo93@users.noreply.github.com>
2023-06-27 15:35:15 +0200

    update model

--- models/models.py
+++ models/models.py
@@ -32,31 +32,30 @@ class CNN(nn.Module):
 class BatchNormCNN(nn.Module):
     def __init__(self, n_classes: int, in_channels: int):
         super().__init__()
-        self.conv1 = nn.Conv2d(in_channels, 10, kernel_size=5)
-        self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
+        self.conv1 = nn.Conv2d(in_channels, 10, kernel_size=5, bias=False)
+        self.conv2 = nn.Conv2d(10, 20, kernel_size=5, bias=False)
         self.dropout2d = nn.Dropout2d()
         self.dropout = nn.Dropout()
         self.max_pool2d = nn.MaxPool2d(2)
-        self.batch_norm2d = nn.BatchNorm2d(20)
+        self.batch_norm2d1 = nn.BatchNorm2d(20)
+        self.batch_norm2d2 = nn.BatchNorm2d(20)
         self.fc1 = nn.Linear(320, 32)  # nn.Linear(6480, 32)
         self.fc2 = nn.Linear(32, n_classes)
         self.relu = nn.ReLU()
 
     def forward(self, x: torch.Tensor):
         x = self.conv1(x)  # [batch, 3, 85, 85] [batch, 1, 28, 28]
-        x = self.dropout2d(x)  # [batch, 10, 81, 81] [batch, 10, 24, 24]
-        x = self.max_pool2d(x)  # [batch, 10, 40, 40] [batch, 10, 12, 12]
+        x = self.batch_norm2d1(x)
         x = self.relu(x)
+        x = self.max_pool2d(x)  # [batch, 10, 40, 40] [batch, 10, 12, 12]
 
         x = self.conv2(x)  # [batch, 20, 36, 36] [batch, 20, 8, 8]
-        x = self.dropout2d(x)
-        x = self.max_pool2d(x)  # [batch, 20, 18, 18] [batch, 20, 4, 4]
-        x = self.batch_norm2d(x)
+        x = self.batch_norm2d2(x)
         x = self.relu(x)
+        x = self.max_pool2d(x)  # [batch, 20, 18, 18] [batch, 20, 4, 4]
 
         x = torch.flatten(x, 1)  # [batch, 6480] [batch, 320]
         x = self.fc1(x)  # [batch, 32]
         x = self.relu(x)
-        x = self.dropout(x)
         x = self.fc2(x)  # [batch, 1]
         return x
