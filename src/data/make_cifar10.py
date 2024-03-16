c0b2979e8d87ea6d7d8b138e86cb27868a5c4fa9
Tom <tomliessmann@gmail.com>
2024-03-16 19:37:09 +0100

    Added function for exporting npz from cifar10 to interim data

--- src/data/make_cifar10.py
+++ src/data/make_cifar10.py
@@ -1,19 +1,11 @@
 import os
+import numpy as np
 import torch
 import torchvision
 from torchvision.transforms import ToTensor
 
 
-def main():
-    data_root = os.path.abspath(
-        os.path.join(
-            os.path.dirname(__file__),
-            os.pardir,
-            os.pardir,
-            "data",
-            "raw",
-        )
-    )
+def get_cifar10_dataset(data_root: str = None):
     trainset = torchvision.datasets.CIFAR10(
         root=data_root,
         train=True,
@@ -26,10 +18,40 @@ def main():
         download=False,
         transform=ToTensor(),
     )
+    return trainset, testset
 
-    trainloader = torch.utils.data.DataLoader(trainset)
-    testloader = torch.utils.data.DataLoader(testset)
-    x = 1
+
+def get_cifar10_dataloader(dataset):
+    return torch.utils.data.DataLoader(dataset)
+
+
+def export_cifar10(dataloader, export_path, train=True):
+    fname = "cifar10-train.npz" if train else "cifar10-test.npz"
+    exp_path = os.path.join(export_path, fname)
+    imgs, labels, extra_labels = [], [], []
+    for image, label in dataloader:
+        imgs.append(image.squeeze(0).numpy())
+        labels.append(label.item())
+        extra_labels.append(0)
+
+    npz_arrs = {"data": imgs, "labels": labels, "extra_labels": extra_labels}
+    np.savez_compressed(exp_path, **npz_arrs)
+    return True
+
+
+def main():
+    data_root = os.path.abspath(
+        os.path.join(
+            os.path.dirname(__file__),
+            os.pardir,
+            os.pardir,
+            "data",
+            "raw",
+        )
+    )
+    trainset, testset = get_cifar10_dataset(data_root)
+    trainloader = get_cifar10_dataloader(trainset)
+    testloader = get_cifar10_dataloader(testset)
 
 
 if __name__ == "__main__":
--- tests/test_make_data.py
+++ tests/test_make_data.py
@@ -1,9 +1,104 @@
-from src.data.make_dataset import DataManager
-from torchvision.transforms import ToTensor
+import os
 import numpy as np
 import torchvision
-
+from torchvision.transforms import ToTensor
+from src.data.make_dataset import DataManager
+from src.data.make_cifar10 import get_cifar10_dataset, get_cifar10_dataloader, export_cifar10
 import pytest
+
+
+class TestCifar10:
+    @pytest.fixture
+    def data_root(self):
+        yield os.path.abspath(
+            os.path.join(
+                os.path.dirname(__file__),
+                os.pardir,
+                "data",
+                "raw",
+            )
+        )
+
+    @pytest.fixture
+    def data_interim_path(self):
+        yield os.path.abspath(
+            os.path.join(os.path.dirname(__file__), os.pardir, "data", "interim", "cifar10")
+        )
+
+    @pytest.fixture
+    def cifar10_interim_data(self):
+        data_path = os.path.abspath(
+            os.path.join(
+                os.path.dirname(__file__),
+                os.pardir,
+                "data",
+                "interim",
+                "cifar10",
+                "cifar10-test.npz",  # "poison_cifar10-test.npz",
+            )
+        )
+        data = np.load(data_path, allow_pickle=False)
+        yield data
+
+    @pytest.fixture
+    def cifar10_poison_data(self):
+        data_path = os.path.abspath(
+            os.path.join(
+                os.path.dirname(__file__),
+                os.pardir,
+                "data",
+                "processed",
+                "cifar10",
+                "poison_cifar10-test.npz",
+            )
+        )
+        data = np.load(data_path, allow_pickle=False)
+        yield data
+
+    @pytest.fixture
+    def expected_cifar_label_dist(self):
+        yield {}
+
+    def test_read_cifar10_data(self, data_root):
+        train, test = get_cifar10_dataset(data_root)
+        assert train is not None
+        assert test is not None
+        train_loader = get_cifar10_dataloader(train)
+        test_loader = get_cifar10_dataloader(test)
+        assert train_loader is not None
+        assert test_loader is not None
+
+    @pytest.mark.skip(reason="Skipping because of real data time overhead")
+    def test_create_cifar10_interim_data(self, data_root, data_interim_path):
+        train, test = get_cifar10_dataset(data_root)
+        train_dl, test_dl = get_cifar10_dataloader(train), get_cifar10_dataloader(test)
+        train_success = export_cifar10(train_dl, data_interim_path, train=True)
+        test_success = export_cifar10(test_dl, data_interim_path, train=False)
+        assert train_success is True
+        assert test_success is True
+
+    def test_cifar10s_data_has_two_labels(self, cifar10_interim_data):
+        assert cifar10_interim_data["labels"] is not None
+        assert cifar10_interim_data["data"] is not None
+        assert cifar10_interim_data["extra_labels"] is not None
+
+    @pytest.mark.xfail(reason="Not yet implemented")
+    def test_cifar10_poison_data_has_correct_label_dist(
+        self, cifar10_poison_data, expected_cifar_label_dist
+    ):
+        """Distribution"""
+        dist = check_label_dist(cifar10_poison_data)
+        assert dist == expected_cifar_label_dist
+
+    @pytest.mark.xfail(reason="Not yet implemented")
+    def test_cifar_npz_is_same_as_cifar_pytorch(self):
+        # custom_cifar = CifarDataset("data/raw/cifar-10-batches-py")
+        torch_cifar = trainset = torchvision.datasets.CIFAR10(
+            root=None,
+            train=True,
+            download=False,
+            transform=ToTensor(),
+        )
 
 
 class TestDataManager:
@@ -46,39 +141,3 @@ class TestDataManager:
 
     def test_store(self, data_manager):
         pass
-
-
-@pytest.fixture
-def cifar10_poison_data():
-    data_path = "data/processed/cifar10/poison_cifar10-test.npz"
-    data = np.load(data_path)
-    yield data
-
-
-def test_cifar10_poison_data_has_two_labels(cifar10_poison_data):
-    assert cifar10_poison_data["labels"] is not None
-    assert cifar10_poison_data["data"] is not None
-    assert cifar10_poison_data["extra_labels"] is not None
-
-
-def test_cifar10_poison_data_has_correct_label_dist():
-    """Distribution"""
-    pass
-
-
-def test_read_cifar10_data():
-    """Should verify if the original cifar10 data was read into memory"""
-    data_path = "data/raw/cifar-10-batches-py"
-    data = np.load(data_path)
-    assert data is not None
-
-
-@pytest.mark.xfail(reason="Not yet implemented")
-def test_cifar_npz_is_same_as_cifar_pytorch():
-    # custom_cifar = CifarDataset("data/raw/cifar-10-batches-py")
-    torch_cifar = trainset = torchvision.datasets.CIFAR10(
-        root="/home/bay1989/masterarbeit/backdoor",
-        train=True,
-        download=False,
-        transform=ToTensor(),
-    )
